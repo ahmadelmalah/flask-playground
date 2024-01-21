9ceed0525beba7f3f0b2a265f7e783fd7fba95d6
Ahmad Elmalah <ahmedelmalah@hotmail.com>
2024-01-22 01:14:04 +0200

    Adding an html route

--- app.py
+++ app.py
@@ -8,6 +8,20 @@ def hello():
     resp['message'] = 'Hello, World!'
     return resp
 
+@app.route('/html/')
+def hello_world(user=None):
+    user = user or 'Ahmad'
+    return '''
+<html>
+    <head>
+        <title>Flask Framework Cookbook</title>
+    </head>
+    <body>
+        <h1>Hello %s!</h1>
+        <p>Welcome to the world of Flask!</p>
+    </body>
+</html>''' % user
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
